86fdbdfe25d4aae00d651e8cc3ce5736ab90b4fe
Chad Hollandsworth <CHollandsworth@AdvantageTech.biz>
2019-12-09 12:44:08 -0500

    Add project files.

---
+++ xamarinTest/xamarinTest/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Android.App;
+using Android.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace xamarinTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        int count = 0;
+        void Button_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"You clicked {count} times.";
+        }
+
+        void aboutScreen(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new aboutScreen());
+        }
+
+        void votingScreen(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new votingScreen());
+        }
+
+        void hostScreen(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new hostScreen());
+        }
+
+        void takeActionScreen(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new takeAction());
+        }
+
+        
+
+
+    }
+}
